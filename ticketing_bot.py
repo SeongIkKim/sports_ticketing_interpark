779c1637da1fcd296902684ffdee9c0121694d8f
SeongIkKim <kpic1638@gmail.com>
2020-11-01 22:00:39 +0900

    좌석 selecting까지 완료

--- ticketing_bot.py
+++ ticketing_bot.py
@@ -73,9 +73,13 @@ def select_match(driver):
         print(e)
         return None
 
-def get_link_for_match(time_schedule):
-    ticketing_btn = time_schedule.find_element_by_class_name('btns').find_element_by_css_selector('a')
-    ticketing_btn.send_keys(Keys.ENTER)
+def btn_click(btn):
+    btn.send_keys(Keys.ENTER)
+
+def click_ticketing_link_for_match(match):
+    ticketing_btn = match.find_element_by_class_name('btns').find_element_by_css_selector('a')
+    btn_click(ticketing_btn)
+
 
 def login(driver, login_url, site_url):
     driver.get(login_url)
@@ -94,7 +98,24 @@ def login(driver, login_url, site_url):
 
     driver.get(site_url)
 
+def switch_to_popup_window(driver):
+    child_window = driver.window_handles[-1]
+    # 팝업창으로 전환
+    try:
+        driver.switch_to.window(child_window)
+    except Exception as e:
+        print(e)
 
+def switch_to_target_iframe(driver, iframe_id):
+    while True:
+        try:
+            target_iframe = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.ID, iframe_id))
+            )
+            break
+        except:
+            continue
+    driver.switch_to.frame(target_iframe)
 
 
 
@@ -128,19 +149,81 @@ driver = webdriver.Chrome('./chromedriver', options=options)
 
 
 
-#### 쿠키생성 ####
+### 드라이버 로그인 ###
 login(driver,login_url=LOGIN_URL,site_url=SITE_URL)
 
 # 매치 선택
 match = select_match(driver)
 if match is None:
     print("선택한 경기를 찾을 수 없습니다.")
+# -------------직링 생성--------------
+# GROUP_CODE = '20007809'
+# PLAY_DATE = '20201106'
+# PLAY_SEQ = '001'
+# BIZ_MEMBER_CODE = '116737815'
+#
+# driver.get(f'http://poticket.interpark.com/Book/BookSession.asp?GroupCode={GROUP_CODE}&Tiki=N&Point=N&PlayDate={PLAY_DATE}&PlaySeq={PLAY_SEQ}&BizCode=&BizMemberCode={BIZ_MEMBER_CODE}')
+# -------------직링 생성끝--------------
+
+
+parent_window = driver.current_window_handle
 
 # 예매하기 버튼 누르기
-get_link_for_match(match)
+click_ticketing_link_for_match(match)
 
-print('끝')
+switch_to_popup_window(driver)
 
+# 좌석 종류 선택 프레임으로 스위치
+switch_to_target_iframe(driver, 'ifrmSeat')
+
+# 좌석 종류 리스트 뽑아오기
+available_seat_types = []
+try:
+    seat_types = driver.find_elements_by_xpath('/html/body/div/div[3]/div[2]/div[1]/a')
+    for seat_type in seat_types:
+        sgn = seat_type.get_attribute('sgn')
+        rc = seat_type.get_attribute('rc')
+        if int(rc) > 0 : # Todo 안정성을 위해 조정가능(2나 3 정도로)
+            available_seat_types.append(seat_type)
+except Exception as e:
+    print(e)
+
+# 좌석 종류 선택 후 다음창으로 넘어가기
+for seat_type in available_seat_types:
+    try:
+        btn_click(seat_type)
+        select_btn = driver.find_element_by_xpath('/html/body/div/div[3]/div[2]/div[3]/a')
+        btn_click(select_btn)
+        break
+    except Exception as e:
+        print(e)
+        print('해당 좌석 타입이 선택되지 않습니다.')
+
+# 세부 좌석 선택 프레임으로 스위치
+switch_to_target_iframe(driver, 'ifrmSeatDetail')
+
+# 세부 좌석 선택
+# seats = WebDriverWait(driver, 3).until(
+#                 EC.presence_of_all_elements_located((By.CLASS_NAME, 'stySeat'))
+#             )
+seats = driver.find_elements_by_class_name('stySeat')
+selected = False
+while not selected:
+    for seat in seats:
+        try:
+            if seat.get_attribute('title')[6:8] == 'S8': # Todo 원하는 좌석위치
+                driver.execute_script('arguments[0].click();', seat)
+                print(1)
+                selected = True
+                break
+        except Exception as e:
+            print(e)
+
+# 프레임 바꿔서 좌석선택완료 누르기
+driver.switch_to.default_content()
+switch_to_target_iframe(driver,'ifrmSeat')
+selection_complete_btn = driver.find_element_by_xpath('/html/body/form[1]/div/div[1]/div[3]/div/div[4]/a')
+btn_click(selection_complete_btn)
 
 
 
